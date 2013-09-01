11c627a67ade30de454d4813e34b7d3d8c7bad6a
Tian <Tian@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-01 12:26:44 +0000

    Flexform renamed to plugin options.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cb_wishlist/trunk@79386 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -75,6 +75,6 @@ $pluginName = strtolower('Cbwishlist');
 $pluginSignature = $extensionName.'_'.$pluginName; 
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages'; 
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_list.xml'); 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/flexform_plugin_options.xml'); 
 
 ?>
